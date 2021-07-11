3e3316f2c7364c0f32dd5769b5826dd77b2e280d
Rachelle Lee <rachedeveloper@gmail.com>
2021-07-11 15:19:09 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by RachelleLee on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
